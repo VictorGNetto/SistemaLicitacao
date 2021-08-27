6ff418500caff3eefc904812c1590a164ab389de
VictorGNetto <victornetto@discente.ufg.br>
2021-08-27 08:05:39 -0300

    ajustado configurações de proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
     {
         context: ['/api'],
-        target: 'http://localhost:80/SL',
+        target: 'http://localhost:80/SL-backend',
         secure: false,
         logLevel: 'debug',
         pathRewrite: { '^/api': ''},
