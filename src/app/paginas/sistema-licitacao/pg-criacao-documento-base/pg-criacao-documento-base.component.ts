21ed22763ecaab055ec34c14a3410130674307c9
VictorGNetto <victornetto@discente.ufg.br>
2021-09-20 15:41:49 -0300

    adicionar Seção; remover Seção; nomear Seção; navegação entre Seções

--- src/app/paginas/sistema-licitacao/pg-criacao-documento-base/pg-criacao-documento-base.component.ts
+++ src/app/paginas/sistema-licitacao/pg-criacao-documento-base/pg-criacao-documento-base.component.ts
@@ -9,12 +9,32 @@ export class PgCriacaoDocumentoBaseComponent implements OnInit {
 
   nomeDocumentoBase: string = "";
   secoes: string[] = [
-    ""
+    "1ª Seção"
   ];
+  secaoSelecionada = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  adicionarSecao() {
+    let qtdSecoes = this.secoes.length;
+    this.secoes.push(`${qtdSecoes + 1}ª Seção`);
+    console.log(this.secoes);
+  }
+
+  removerSecao() {
+    this.secoes.splice(this.secaoSelecionada, 1);
+  }
+
+  salvaNomeSecao(index: number, event: FocusEvent) {
+    let target = event.target as HTMLInputElement;
+    this.secoes[index] = target.value;
+  }
+
+  onMudancaSecao(selectedIndex: number) {
+    this.secaoSelecionada = selectedIndex;
   }
 
 }
