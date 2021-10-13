f35ae188677bd05756e255ca4f5b1ad9cff89550
VictorGNetto <victornetto@discente.ufg.br>
2021-10-13 17:32:29 -0300

    Iniciado o layout do Item Opções

--- src/app/paginas/sistema-licitacao/componentes/item-opcoes/item-opcoes.component.ts
+++ src/app/paginas/sistema-licitacao/componentes/item-opcoes/item-opcoes.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { EventEmitter, Input, Output } from '@angular/core';
 
+interface Subitem {
+  tipo: string,                     // "subdescricao", "opcao" e "opcao-entrada-texto"
+  subdescricao?: string             // aplicável somente quando tipo="subdescricao"
+  opcao?: string                    // aplicável somente quando tipo="opcao" ou "opcao-entrada-texto"
+  placeholderEntradaTexto?: string  // aplicável somente quando tipo="opcao-entrada-texto"
+}
+
 @Component({
   selector: 'item-opcoes',
   templateUrl: './item-opcoes.component.html',
@@ -10,6 +17,18 @@ export class ItemOpcoesComponent implements OnInit {
 
   @Input() itemID = "";
   @Input() salvarItem = false;
+
+  nivelIndentacao = 0; // 0, 1 ou 2
+  nivelIndentacaoClass = "container-0";
+
+  descricao = "Descrição";
+  subitens: Subitem[] = [
+    { tipo: "subdescricao", subdescricao: "123" },
+    { tipo: "opcao", opcao: "456" },
+    { tipo: "opcao-entrada-texto", opcao: "789", placeholderEntradaTexto: "000" }
+  ];
+
+  modoPrevisualizacao = false;
 
   @Output() salvado = new EventEmitter<void>();
 
@@ -23,6 +42,19 @@ export class ItemOpcoesComponent implements OnInit {
       console.log("Salvando item " + this.itemID);
       this.salvado.emit();
     }
+  }
+
+  mudarNivelIndentacao(novoNivelIndentacao: number) {
+    this.nivelIndentacao = novoNivelIndentacao;
+    this.nivelIndentacaoClass = `container-${novoNivelIndentacao}`;
+  }
+
+  removerSubitem(index: number) {
+    this.subitens.splice(index, 1);
+  }
+
+  tooglePrevisualizacao() {
+    this.modoPrevisualizacao = !this.modoPrevisualizacao;
   }
 
 }
