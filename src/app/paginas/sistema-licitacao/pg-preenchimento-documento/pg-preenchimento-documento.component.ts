d1fa3d3d1bd9f71c62d62e439bafd363dec92117
VictorGNetto <victornetto@discente.ufg.br>
2021-10-28 17:27:47 -0300

    parte significativa do .html e .css da página de preenchimento de documentos finalizada

--- src/app/paginas/sistema-licitacao/pg-preenchimento-documento/pg-preenchimento-documento.component.ts
+++ src/app/paginas/sistema-licitacao/pg-preenchimento-documento/pg-preenchimento-documento.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Item {
+  tipo: string;
+  itemID: string;
+}
+
+interface Secao {
+  nome: string;
+  itens: Item[];
+}
+
 @Component({
   selector: 'app-pg-preenchimento-documento',
   templateUrl: './pg-preenchimento-documento.component.html',
   styleUrls: ['./pg-preenchimento-documento.component.css']
 })
 export class PgPreenchimentoDocumentoComponent implements OnInit {
+  nomeDocumento = "Termo de Referência - Compra Direta";
+  documentoID = "";
+  secoes: Secao[] = [
+    {
+      nome: '1ª Seção',
+      itens: [
+        {"tipo":"texto","itemID":"lrpraz"},
+        {"tipo":"nota","itemID":"qynupj"},
+        {"tipo":"opcoes","itemID":"mgnfyg"},
+      ],
+    },
+    {
+      nome: '2ª Seção',
+      itens: [
+        // { tipo: 'nota', itemID: 'item novo 255' },
+        // { tipo: 'texto', itemID: 'item novo 256' },
+        // { tipo: 'opcoes', itemID: 'item novo 257' },
+      ],
+    },
+  ];
+  secaoSelecionada = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // --------------------------------------------------------------------------
+  // Funções e Variáveis usadas no processo de salvamento do Documento Base ---
+  // --------------------------------------------------------------------------
+  salvandoItens = false;
+
+  salvarItens() {
+  }
+
+  itenFoiSalvo() {
+
   }
 
 }
